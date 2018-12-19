56f6965d736cb3f79c573dec45a3251ca59e0a6a
AIT BAHA Anas <aitbaha.ans@gmail.com>
2018-12-19 12:39:49 +0100

    configuratio of the company service

--- company-service/src/main/java/me/aitbahaa/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/me/aitbahaa/companyservice/CompanyServiceApplication.java
@@ -1,14 +1,29 @@
 package me.aitbahaa.companyservice;
 
+import me.aitbahaa.companyservice.dao.CompanyRepository;
+import me.aitbahaa.companyservice.entities.Company;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CompanyServiceApplication {
+    @Autowired
+    private CompanyRepository companyRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CompanyServiceApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    private void startup() {
+        Stream.of("Sango", "Apple", "Samsung", "Xiaomi", "Microsoft", "HP", "Toshiba", "Asus", "MSI")
+                .map(companyName -> new Company(null, companyName))
+                .forEach(companyRepository::save);
+    }
 }
 
---
+++ company-service/src/main/java/me/aitbahaa/companyservice/dao/CompanyRepository.java
@@ -0,0 +1,10 @@
+package me.aitbahaa.companyservice.dao;
+
+import me.aitbahaa.companyservice.entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+}
