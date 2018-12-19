4e8ecd47a0dc7bd040717a0f661a97c7f45353f3
AIT BAHA Anas <aitbaha.ans@gmail.com>
2018-12-19 12:28:13 +0100

    Initial commit

---
+++ eureka-register-service/src/main/java/me/aitbahaa/eurekaregisterservice/EurekaRegisterServiceApplication.java
@@ -0,0 +1,14 @@
+package me.aitbahaa.eurekaregisterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegisterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegisterServiceApplication.class, args);
+    }
+
+}
+
