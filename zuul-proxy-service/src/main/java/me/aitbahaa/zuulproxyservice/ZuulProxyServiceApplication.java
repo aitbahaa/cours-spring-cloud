fbb13ac5266c3e8dbc98ecbe14209424d66a3c6a
AIT BAHA Anas <aitbaha.ans@gmail.com>
2018-12-19 12:33:18 +0100

    Initial commit

---
+++ zuul-proxy-service/src/main/java/me/aitbahaa/zuulproxyservice/ZuulProxyServiceApplication.java
@@ -0,0 +1,14 @@
+package me.aitbahaa.zuulproxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyServiceApplication.class, args);
+    }
+
+}
+
