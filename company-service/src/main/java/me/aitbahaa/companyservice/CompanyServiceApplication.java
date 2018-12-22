e8e3c70b88ab73132e2ef92637fed3321c1073a8
AIT BAHA Anas <aitbaha.ans@gmail.com>
2018-12-22 02:27:09 +0100

    company service :: enable the discovery by eureka

--- company-service/src/main/java/me/aitbahaa/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/me/aitbahaa/companyservice/CompanyServiceApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.event.EventListener;
 
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyServiceApplication {
     @Autowired
