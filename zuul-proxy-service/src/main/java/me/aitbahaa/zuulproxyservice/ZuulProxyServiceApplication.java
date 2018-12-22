b82df8f8154a5029e292180e84706af205cf344f
AIT BAHA Anas <aitbaha.ans@gmail.com>
2018-12-22 02:34:23 +0100

    set-up zuul-proxy-service

--- zuul-proxy-service/src/main/java/me/aitbahaa/zuulproxyservice/ZuulProxyServiceApplication.java
+++ zuul-proxy-service/src/main/java/me/aitbahaa/zuulproxyservice/ZuulProxyServiceApplication.java
@@ -2,7 +2,12 @@ package me.aitbahaa.zuulproxyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulProxyServiceApplication {
 
